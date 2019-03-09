f7b4e4008345e929cc96f18419152f1c306a1c22
Antonio Kristicevic <kristicevic.antonio@gmail.com>
2019-03-09 19:27:23 +0100

    Add using statements and namespace

--- ExampleServiceTests.cs
+++ ExampleServiceTests.cs
@@ -1,17 +1,27 @@
-[TestFixture]
-public class ExampleServiceTests
-	private Mock<IRepository<Employee>> _studentRepository;
-	private Mock<IRepository<StudentCourse>> _studentCourseRepository;
-	private Mock<IRepository<Course>> _courseRepository;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Moq;
 
-	private ExampleService;
-
-	[SetUp]
-	public void SetUp()
+namespace InsertProjectNameHere.Tests
+{
+	[TestFixture]
+	public class ExampleServiceTests
 	{
-		_studentRepository = new Mock<IRepository<Employee>>();
-		_studentCourseRepository = new Mock<IRepository<StudentCourse>>();
-		_courseRepository = new Mock<IRepository<Course>>();
-	
-		ExampleService = new ExampleService(_studentRepository.Object,_studentCourseRepository.Object,_courseRepository.Object);
+		private Mock<IRepository<Employee>> _studentRepository;
+		private Mock<IRepository<StudentCourse>> _studentCourseRepository;
+		private Mock<IRepository<Course>> _courseRepository;
+
+		private ExampleService _service;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_studentRepository = new Mock<IRepository<Employee>>();
+			_studentCourseRepository = new Mock<IRepository<StudentCourse>>();
+			_courseRepository = new Mock<IRepository<Course>>();
+		
+			_service = new ExampleService(_studentRepository.Object,_studentCourseRepository.Object,_courseRepository.Object);
+		}
 	}
+}
--- main.py
+++ main.py
@@ -1,86 +1,99 @@
-import re
-import os
-#Get file as cmd argument later
-    #Handle errors( doesnt exist, isnt .cs or something else)
-#Parse file and get all the fields
-    #Find all the lines matching private/public readonly Type _name;
-    #Named convention helps a lot I guess
-#Generate Mock fields and initialize the from setup
-#Add correct boilerplate stuff
+import re,os,sys
 
-service_file_path="ExampleService.cs"
+def validate_input_parameter(file_path):
+    if(not os.path.exists(file_path)):
+        raise ValueError("File path:\""+file_path+"\"does not exist!")
+
 
 def generate_unit_test_file(service_file_path):
-    service_name=os.path.basename(os.path.normpath(service_file_path))[:-3]
-    unit_test_name= generate_unit_test_name(service_file_path)
-    injected_dependencies=get_injected_dependencies()
-    mocked_declarations=generate_mocked_declarations(injected_dependencies)  
-    setup_method_statements=generate_setup_method(mocked_declarations,service_name)
+    validate_input_parameter(file_path)
+    service_name = os.path.basename(os.path.normpath(service_file_path))[:-3]
+    unit_test_name = generate_unit_test_name(service_file_path)
+    injected_dependencies = get_injected_dependencies(service_file_path)
+    mocked_declarations = generate_mocked_declarations(injected_dependencies)
+    setup_method_statements = generate_setup_method(mocked_declarations, service_name)
 
     with open(unit_test_name, "w") as f:
-        f.write('[TestFixture]\n')
-        f.write(f'public class {unit_test_name[:-3]}\n')
+        f.write('using FluentAssertions;\nusing NUnit.Framework;\nusing System.Collections.Generic;\nusing Moq;\n\n')
+        f.write('namespace InsertProjectNameHere.Tests\n{\n')
+        f.write('\t[TestFixture]\n')
+        f.write(f'\tpublic class {unit_test_name[:-3]}\n')
+        f.write('\t{\n')
         for declaration in mocked_declarations:
-            f.write(f'\t{declaration}\n')
+            f.write(f'\t\t{declaration}\n')
         f.write('\n')
-        f.write(f'\tprivate {service_name};\n\n')
+        f.write(f'\t\tprivate {service_name} _service;\n\n')
         for statement in setup_method_statements:
-            f.write(f'\t{statement}\n')
+            f.write(f'\t\t{statement}\n')
+        f.write('\t}\n}')
 
-    
-def get_injected_dependencies():
-    injected_dependencies=[]
+
+def get_injected_dependencies(service_file_path):
+    injected_dependencies = []
     for line in open(service_file_path, 'r'):
         if (is_injected_dependency(line)):
             injected_dependencies.append(line)
     return injected_dependencies
 
+
 def is_injected_dependency(line):
-    line=line.strip()
-    return re.search(r'private[\s]readonly[\s][^ ]+[\s]_[^ ;]+;',line)
+    line = line.strip()
+    return re.search(r'private[\s]readonly[\s][^ ]+[\s]_[^ ;]+;', line)
+
 
 def generate_mocked_declarations(injected_dependencies):
-    mocked_declarations=[]
+    mocked_declarations = []
 
     for injected_dependency in injected_dependencies:
-        statement_segments=injected_dependency.split()
-        statement_segments[2]=f'Mock<{statement_segments[2]}>'
+        statement_segments = injected_dependency.split()
+        statement_segments[2] = f'Mock<{statement_segments[2]}>'
         statement_segments.remove('readonly')
 
         mocked_declarations.append(' '.join(statement_segments))
     return mocked_declarations
 
+
 def generate_unit_test_name(service_file_path):
-    service_name=os.path.basename(os.path.normpath(service_file_path))
+    service_name = os.path.basename(os.path.normpath(service_file_path))
     return service_name[:-3] + 'Tests' + service_name[-3:]
 
-def generate_setup_method(mocked_declarations,service_name):
-    lines=[]
+
+def generate_setup_method(mocked_declarations, service_name):
+    lines = []
     lines.append('[SetUp]')
     lines.append('public void SetUp()')
     lines.append('{')
 
     for declaration in mocked_declarations:
-        statement_segments=declaration.split()
-        dependency_name=statement_segments[-1][:-1]
-        mocked_declaration_type=statement_segments[1]
-        
+        statement_segments = declaration.split()
+        dependency_name = statement_segments[-1][:-1]
+        mocked_declaration_type = statement_segments[1]
+
         lines.append(f'\t{dependency_name} = new {mocked_declaration_type}();')
 
-    constructor_statement=generate_constructor_statement(service_name,mocked_declarations)
+    constructor_statement = generate_constructor_statement(service_name,mocked_declarations)
     "constructor:"+constructor_statement
-    lines.append(f'\n\t\t{constructor_statement}')
+    lines.append(f'\n\t\t\t{constructor_statement}')
     lines.append('}')
-    return lines;
+    return lines
 
-def generate_constructor_statement(service_name, mocked_declarations):
-    parameters=[]
+
+def generate_constructor_statement(service_name,mocked_declarations):
+    parameters = []
     for declaration in mocked_declarations:
-        declaration_segments=declaration.split();
-        dependency_name=declaration_segments[-1][:-1]
+        declaration_segments = declaration.split()
+        dependency_name = declaration_segments[-1][:-1]
         parameters.append(f'{dependency_name}.Object')
-    
-    comma_separated_parameters=','.join(parameters)
-    return f'{service_name} = new {service_name}({comma_separated_parameters});'
 
-generate_unit_test_file(service_file_path)
+    comma_separated_parameters = ','.join(parameters)
+    return f'_service = new {service_name}({comma_separated_parameters});'
+
+if(sys.argv.__len__()<2):
+    print(".cs file should be provided as argument.")
+else:
+    file_path = sys.argv[1]
+
+    try:
+        generate_unit_test_file(file_path)
+    except ValueError as error:
+        print(error)
